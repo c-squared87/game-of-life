4093aa21b607b4d21c76347294b71977b1838af3
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-08-26 10:04:50 -0500

    cleaning up checkForValidity

--- main.js
+++ main.js
@@ -30,92 +30,109 @@ console.log('hello world')
 
 class GameController {
 
-    constructor() {
-        this.cells = []
-        this.generations = 0;
-        this.generateGrid()
-    }
+	constructor() {
+		this.cells = []
+		this.generations = 0;
 
-    generateGrid() {
-        for (let x = 0; x < 5; x++) {
-            this.cells[x] = []
-            for (let y = 0; y < 5; y++) {
-                this.cells[x].push(new Cell(x, y, false))
-            }
-        }
-        // console.log(this.cells)
-    }
+		this.generateGrid()
+	}
 
-    cycleGeneration() { // TODO: DUPLICATE A GRID THEN REASSIGN AT END.
-        // for (let x = 0; x < this.cells.length; x++) {
-        //     for (let y = 0; y < this.cells[x].length; y++) {
-        //         if (this.cells[x][y]._active) {
-        //             this.checkForValidity(x, y)
-        //         }
-        //     }
-        // }
-        this.generations++
-        this.printToConsole();
-    }
+	generateGrid() {
+		this.cells = []
+		for (let x = 0; x < 5; x++) {
+			this.cells[x] = []
+			for (let y = 0; y < 5; y++) {
+				this.cells[x].push(new Cell(x, y, false))
+			}
+		}
 
-    printToConsole() {
-        console.log('current gen ' + this.generations)
-        var _str = '[ '
-        for (let i = 0; i < this.cells[i].length - 1; i++) {
-            _str = '[ '
-            for (let j = 0; j < this.cells[i].length - 1; j++) {
-                _str += this.cells[i][j].drawCell()
-            }
-            console.log(_str + ' ]' + i)
-        }
-    }
+	}
 
-    //FIXME:
-    checkForValidity(_cellX, _cellY) {
-        let _neighbors = 0
-        for (let x = -1; x < 2; x++) {
-            for (let y = -1; y < 2; y++) {
-                if (this.cells[_cellX + x][_cellY + y] != undefined) {
-                    // console.log(this.cells[_cellX + x][_cellY + y]._active)
-                    if (this.cells[_cellX + x][_cellY + y]._active) {
-                        _neighbors++
-                    }
-                }
-            }
-        }
-        if (_neighbors <= 1 || _neighbors >= 4) {
-            // console.log('died');
-            this.cells[_cellX, _cellY]._active = false;
-        }
-        // this.cells[_cellX][_cellY].draw()
-    }
+	cycleGeneration() { // TODO: DUPLICATE A GRID THEN REASSIGN AT END.
+		var _local = this.cells;
+
+		// THIS VALIDITY CHECK IS A SHIT SHOW.
+
+		for (let x = 0; x < _local.length; x++) {
+			for (let y = 0; y < _local.length; y++) {
+				if (_local[x][y]._active) {
+					// this.checkForValidity(x, y)
+					if (!this.checkForValidity(x, y)) {
+						_local[x][x].toggleActive()
+					}
+				} else {
+					if (this.checkForValidity(x, y)) {
+						_local[x][x].toggleActive()
+					}
+				}
+			}
+		}
+
+		console.log(this.cells)
+		this.cells = _local
+
+		this.generations++
+		this.printToConsole();
+	}
+
+	printToConsole() {
+		console.log('current gen ' + this.generations)
+		var _str = '[ '
+		for (let i = 0; i < this.cells[i].length - 1; i++) {
+			_str = '[ '
+			for (let j = 0; j < this.cells[i].length - 1; j++) {
+				_str += this.cells[i][j].drawCell()
+			}
+			console.log(_str + ' ]' + i)
+		}
+	}
+
+	//FIXME: maybe this shoudl return a bool? that can be used to set the cell value in _local
+	checkForValidity(_cellX, _cellY) {
+		let _neighbors = 0
+		for (let x = -1; x < 2; x++) {
+			for (let y = -1; y < 2; y++) {
+				if (x != 0 && y != 0) {
+					if (this.cells[_cellX + x][_cellY + y] != undefined) { // THIS IS DOING SOMETHING WEIRD...
+						// if (this.cells[_cellX + x][_cellY + y]._active) {
+						// 	_neighbors++
+						// }
+					}
+				}
+			}
+		}
+		if (_neighbors <= 1 || _neighbors >= 4) {
+			console.log('died');
+			return false;
+		}
+		return true
+	}
 }
 
 class Cell {
-    snow
 
-    constructor(_x, _y, _active) {
+	constructor(_x, _y, _active) {
 
-        this._x = _x
-        this._y = _y
-        this._active = _active
+		this._x = _x
+		this._y = _y
+		this._active = _active
 
-        this.name = this._x.toString() + this._y.toString();
-    }
+		this.name = this._x.toString() + this._y.toString();
+	}
 
-    toggleActive() {
-        this._active = this._active ? false : true
-    }
+	toggleActive() {
+		this._active = this._active ? false : true
+	}
 
-    drawCell() {
-        let _symbol = this._active ? 'X' : 'O'
-        return _symbol
-    }
+	drawCell() {
+		let _symbol = this._active ? 'X' : 'O'
+		return _symbol
+	}
 }
 
 const gameController = new GameController()
 const nextButton = document.getElementById('data-next-button')
 
 nextButton.addEventListener('click', function () {
-    gameController.cycleGeneration()
+	gameController.cycleGeneration()
 })
